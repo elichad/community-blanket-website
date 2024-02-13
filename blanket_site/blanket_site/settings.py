9e90921f0a373d6d829b35499a348002bca248da
Eli Chadwick <eli.chadwick.256@gmail.com>
2024-02-13 17:03:15 +0000

    set up STATIC_ROOT

--- blanket_site/blanket_site/settings.py
+++ blanket_site/blanket_site/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATICFILES_DIRS = ["static/"]
+STATIC_ROOT = "/var/www/blanket_site/static/"
 STATIC_URL = "static/"
 
 # Default primary key field type
@@ -129,4 +130,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Media files (image uploads)
 # https://docs.djangoproject.com/en/5.0/topics/files/#managing-files
 MEDIA_ROOT = "/var/www/blanket_site/media/"
-# MEDIA_URL = "localhost/"
+MEDIA_URL = "media/"
