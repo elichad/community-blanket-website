c9bc4be6a4d3084045085c0fe0922b5aaf8bf8a8
Eli Chadwick <eli.chadwick.256@gmail.com>
2024-05-31 16:04:49 +0100

    add help text

--- blanket_site/squares/models.py
+++ blanket_site/squares/models.py
@@ -8,13 +8,25 @@ from contributors.models import Person, get_anonymous_user
 class Square(models.Model):
     """Model for tracking individual contributions."""
 
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, help_text="A name for the square.")
     image = models.ImageField(upload_to="uploads/%Y/%m/%d/")
-    creator = models.CharField(max_length=256)
-    date_created = models.DateField()
+    creator = models.CharField(
+        max_length=256,
+        help_text='Your name, or a pseudonym. This will be displayed publicly - if you\'d rather not include any name at all, write "Anon".',
+    )
+    date_created = models.DateField(
+        help_text="Enter the date when you finished the square, not including adding it to the blanket."
+    )
     date_time_uploaded = models.DateTimeField(auto_now_add=True)
-    description = models.TextField()
-    notes = models.TextField(blank=True)
+    description = models.CharField(
+        max_length=1000,
+        verbose_name="alt_text",
+        help_text='A concise visual description of your square. For example, "red and yellow striped square".',
+    )
+    notes = models.TextField(
+        blank=True,
+        help_text="Anything you'd like others to know about the square - for example, why you chose the design, or what the colours mean.",
+    )
 
     # TODO: fields relating to location in a larger blanket
 
