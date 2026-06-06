d6aa0ecb0061d46a4c1fa5bd33c25270e182e148
Eli Chadwick <eli.chadwick.256@gmail.com>
2026-06-06 13:43:29 +0000

    migrate media to ionos and static to local hosting

--- blanket_site/blanket_site/settings.py
+++ blanket_site/blanket_site/settings.py
@@ -162,22 +162,25 @@ if USE_S3:
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
     AWS_DEFAULT_ACL = None
-    AWS_S3_REGION_NAME = "eu-north-1"
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_REGION_NAME = os.getenv("AWS_DEFAULT_REGION")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-central-3.ionoscloud.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_SIGNATURE_VERSION = "s3v4"
     # s3 static settings
-    AWS_LOCATION = "static"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
-    STATICFILES_STORAGE = "blanket_site.storage_backends.StaticStorage"
+    # AWS_LOCATION = "static"
+    # STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+    # STATICFILES_STORAGE = "blanket_site.storage_backends.StaticStorage"
     # s3 public media settings
-    PUBLIC_MEDIA_LOCATION = "media"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+    # PUBLIC_MEDIA_LOCATION = "media"
+    # MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
     DEFAULT_FILE_STORAGE = "blanket_site.storage_backends.PublicMediaStorage"
 else:
-    STATIC_URL = "static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    # STATIC_URL = "static/"
+    # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     MEDIA_URL = "media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
