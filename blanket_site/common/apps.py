4431a715e7598c2145deea81ef1b82ffaba9c218
Eli Chadwick <eli.chadwick.256@gmail.com>
2026-07-06 16:25:35 +0100

    rename thumbnails -> common

--- blanket_site/common/apps.py
+++ blanket_site/common/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ThumbnailsConfig(AppConfig):
+class CommonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "thumbnails"
+    name = "common"
