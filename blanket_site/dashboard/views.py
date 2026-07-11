a85ea70e33f49c654ad19e3e9a2c2fa80335e71f
Eli Chadwick <eli.chadwick.256@gmail.com>
2026-07-11 14:20:05 +0100

    update homepage and nav for 2026

--- blanket_site/dashboard/views.py
+++ blanket_site/dashboard/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from blankets.models import Blanket, BlanketItem
+from solar_systems.models import SolarSystem, SolarSystemItem
 
 
 def index(request):
     """View function for home page of site."""
 
     # Generate counts of some of the main objects
+    emf_solar_system = None
     try:
-        emf_blanket = Blanket.objects.get(name="EMF Community Blanket")
-        num_items = BlanketItem.objects.filter(blanket=emf_blanket).count()
-    except Blanket.DoesNotExist:
+        emf_solar_system = SolarSystem.objects.get(name="EMF Community Solar System")
+        num_items = SolarSystemItem.objects.filter(
+            solar_system=emf_solar_system
+        ).count()
+    except SolarSystem.DoesNotExist:
         num_items = None
 
-    context = {"num_items": num_items, "object": emf_blanket or None}
+    context = {"num_items": num_items, "object": emf_solar_system}
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, "index.html", context=context)
